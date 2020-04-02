0bc82311c3e4d834d3ef58450ac4ff112a18c55b
Kennedy Cambra-Cho <kennedy.cambracho@gmail.com>
2020-04-01 23:48:11 -1000

    array test

--- sketch.js
+++ sketch.js
@@ -1,6 +1,13 @@
+let w; 
+let h;
+
+let buttonACount = 0; 
 
 function setup() {
   let canvas = createCanvas(windowWidth-windowWidth/9, windowHeight/2); 
+
+  w = width; //width of sketch canv
+  h = height; //height of sketch canv
 
   // link canvas to container in HTML 
   canvas.parent('sketch-container'); // allows css edits
@@ -20,9 +27,21 @@ function removeStart(){
   startButton.parentNode.removeChild(startButton); //identify the parent of startButton and delete child = startButton
 }
 
+
+function addText() {
+  let msg = "Hey there!";
+  textSize(50); 
+  fill(255);
+  text(msg, w/3, h/2);
+
+
+}
+
 function startClick(){
   
   removeStart(); //removes start button
+
+  addText();
 
   let buttonA = document.createElement("button"); // create button
   buttonA.innerHTML = "A"; // add text inside button
@@ -41,9 +60,32 @@ function startClick(){
   buttonB.className = "decision-button";
   buttonB.id = "button-B";
 
+  buttonB.addEventListener("click", clickBtnB);
+
 }
 
 function clickBtnA() {
-  console.log("A");
+  console.log("A"); 
+
+  buttonACount += 1; 
+}
+
+function clickBtnB() {
+  console.log("B");
+}
+
+
+
+
+// testing array - pls don't delete - incomplete
+let buttonText = []; 
+
+for (let index = 0; index < 4; index++) {
+  buttonText.push([]);
+  let x = Math.pow(2, index);
+  for (let innerIndex = 0; innerIndex < x; innerIndex++) {
+    buttonText[index].push(["A", "B"]);
+  }
   
 }
+console.log(buttonText);
