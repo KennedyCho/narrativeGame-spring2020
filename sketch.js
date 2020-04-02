f232af8e60f39d60e36df29b02712b97c47a3252
Kennedy Cambra-Cho <kennedy.cambracho@gmail.com>
2020-04-01 21:21:26 -1000

    link canvas object to html div

---
+++ sketch.js
@@ -0,0 +1,15 @@
+
+function setup() {
+  let canvas = createCanvas(windowWidth-windowWidth/9, windowHeight/2); 
+
+  // link canvas to container in HTML 
+  canvas.parent('sketch-container'); // allows css edits
+
+  background(0); 
+
+}
+
+function draw() {
+  // put drawing code here
+  
+}
