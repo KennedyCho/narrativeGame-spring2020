ca40127ca30b5516d8efa8b022cf278275d1f0ae
Kennedy Cambra-Cho <kennedy.cambracho@gmail.com>
2020-04-01 22:33:28 -1000

    add start button and remove start button func

--- sketch.js
+++ sketch.js
@@ -13,3 +13,19 @@ function draw() {
   // put drawing code here
   
 }
+
+// remove start button function
+function removeStart(){
+  let startButton = document.getElementById("start-button"); //assign button to var
+  startButton.parentNode.removeChild(startButton); //identify the parent of startButton and delete child = startButton
+}
+
+function startClick(){
+  
+  removeStart(); //removes start button
+
+  let buttonA = document.createElement("button"); // create button
+  buttonA.innerHTML = "A"; // add text inside button
+  document.body.appendChild(buttonA); // add to html body
+
+}
