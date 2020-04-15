14340e501bc8a91290ef92623ee9ad15486729ff
Dan Li <dl4224@nyu.edu>
2020-04-15 19:25:23 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,36 @@
-let w;
-let h;
+let w; //sketch width
+let h; //sketch height
 
-let buttonA;
+let buttonA; //startButton converted
 let buttonB;
-let buttonC; //startButton
+let buttonC; 
 
 let currentNode = 0; //index within narrative array currently at
 
-let narrative;
+let narrative; //stores array from JSON
+
+let backImg; //background image
+let character; //assigned based on user choice
+let charImg1; //image path
+let charImg2; //image path
+let charImg3; //image path
+
+let backImg1; 
+let backImg2;
+let backImg3;
+
+let size; 
 
 function preload() {
+  backImg = loadImage('assets/start.jpg');
+  // !!! files used for characters are test files only now 
+  charImg1 = loadImage('assets/1.png');  
+  charImg2 = loadImage('assets/2.png');
+  charImg3 = loadImage('assets/3.png');
+  backImg1 = loadImage('assets/backImg1.jpg');
+  backImg2 = loadImage('assets/backImg2.png');
+  backImg3 = loadImage('assets/backImg3.jpg');
+  
   loadJSON('narrative.json', function (loadedData) {
     narrative = loadedData;
   })
@@ -18,7 +39,7 @@ function preload() {
 function setup() {
   console.log(narrative);
   console.log(currentNode);
-  let canvas = createCanvas(windowWidth, windowHeight*0.7);
+  let canvas = createCanvas(windowWidth, windowHeight*0.995);
 
   w = width; //width of sketch canv
   h = height; //height of sketch canv
@@ -26,13 +47,12 @@ function setup() {
   // link canvas to container in HTML 
   canvas.parent('sketch-container'); // allows css edits
 
-  background(0);
+  background(backImg);
 
   addText();
 
   document.getElementById("start-button").addEventListener("click", startClick);
 }
-
 
 function inputOption(currentNode) {
   console.log(currentNode);
@@ -47,20 +67,14 @@ function addText() { //update scene text
 }
 
 function updateSketch() {
+  background(backImg);
+  character.resize(size[0], size[1]);
+  image(character, w/4.5, h*0.3); //character image
+
+
   // @Linda add code here 
   // GOAL: update background image, character image and audio according to the currentNode
-  //officer
-  // loadImage('assets/officer.png', img => {
-  //   image(img, 0, 0);
-  // });
-  // //girl
-  // loadImage('assets/Average people.png', img => {
-  //   image(img, 0, 0);
-  // });
-  // //doctor
-  // loadImage('assets/doctor.png', img => {
-  //   image(img, 0, 0);
-  // });
+  
 
   // let bgmLove, bgmNovember, bgmPiano, mouseClick;
   // soundFormats('mp3');
@@ -74,13 +88,13 @@ function startClick() {
   currentNode ++;
   addText();// update scene Text
 
+  let title = document.getElementById('title');
+  title.remove();
+
   // buttonA
   buttonA = document.getElementById("start-button");
   buttonA.removeEventListener("click", startClick)
   buttonA.addEventListener("click", clickBtnA)
-
-  
-
   //create choice A and B buttons
   buttonB = document.createElement("button"); // create button
   document.getElementById("decision-container").appendChild(buttonB); // add to html body
@@ -99,19 +113,40 @@ function startClick() {
 }
 
 function clickBtnA() {
+
+  if (currentNode == 1) { 
+    character = charImg1; // assign character image 
+    size = [350, 0];
+    backImg = backImg1;
+  }
   currentNode = narrative[currentNode].buttonRoute[0];
-  inputOption(currentNode);
+  updateSketch();
   addText();
+  inputOption(currentNode);
+
 }
 
 function clickBtnB() {
+  if (currentNode == 1) { 
+    character = charImg2; // assign character image 
+    size = [450, 0];
+    backImg = backImg2;
+  }
   currentNode = narrative[currentNode].buttonRoute[1];
+  updateSketch();
   inputOption(currentNode);
   addText();
 }
 
 function clickBtnC() {
+  if (currentNode == 1) { 
+    character = charImg3; // assign character image 
+    size = [400, 0];
+    backImg = backImg3;
+
+  }
   currentNode = narrative[currentNode].buttonRoute[2];
+  updateSketch();
   inputOption(currentNode);
   addText();
 }
